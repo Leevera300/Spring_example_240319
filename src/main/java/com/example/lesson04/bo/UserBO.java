326d2eaffeabe89b626d20cca9075799ca9ec121
DESKTOP-SES5PE0\user <test@test.com>
2024-06-18 18:51:06 +0900

    240618_수업내용

---
+++ src/main/java/com/example/lesson04/bo/UserBO.java
@@ -0,0 +1,27 @@
+package com.example.lesson04.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.lesson04.domain.User;
+import com.example.lesson04.mapper.UserMapper;
+
+@Service
+public class UserBO {
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	// input: name, yyyymmdd, email, introduce
+	// output: 성공한 행의 갯수 / void
+	
+	public void addUser(String name, String yyyymmdd, String email, String introduce) {
+		userMapper.insertUser(name, yyyymmdd, email, introduce);
+	}
+	
+	// input: X
+	// output: User
+	public User getLatestUser() {
+		return userMapper.selectLatestUser();
+	}
+}
