61e82f4716a004164e7de94dea7efed0140bb184
DESKTOP-SES5PE0\user <test@test.com>
2024-06-19 18:51:59 +0900

    240619_수업내용

---
+++ src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
@@ -0,0 +1,41 @@
+package com.example.lesson04;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.lesson04.bo.StudentBO;
+import com.example.lesson04.domain.Student;
+
+@RequestMapping("/lesson04/ex02")
+@Controller
+public class Lesson04Ex02Controller {
+
+	@Autowired
+	private StudentBO studentBO;
+	
+	// 학생 가입 화면
+	// http://localhost/lesson04/ex02/add-student-view
+	
+	@GetMapping("/add-student-view")
+	public String addStudentView() {
+		return "lesson04/addStudent";
+	}
+	// DB insert => 방금 가입된 학생 select
+	@PostMapping("/add-student")
+	public String addStudent(
+			@ModelAttribute Student student) {
+		
+		// DB insert
+		studentBO.addStudent(student);
+		// DB select => 방금 가입된 학생
+		
+		// Model에 담기
+		
+		// 하면 이동
+		return "lesson04/afterAddStudent";
+	}
+}
