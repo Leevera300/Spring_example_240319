7ee1c50f3c4bac6feb9c5f08088521caac7b24a7
DESKTOP-SES5PE0\user <test@test.com>
2024-06-20 18:51:08 +0900

    240620_수업내용

--- src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
@@ -2,6 +2,7 @@ package com.example.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,14 +28,16 @@ public class Lesson04Ex02Controller {
 	// DB insert => 방금 가입된 학생 select
 	@PostMapping("/add-student")
 	public String addStudent(
-			@ModelAttribute Student student) {
+			@ModelAttribute Student student,
+			Model model) {
 		
 		// DB insert
 		studentBO.addStudent(student);
 		// DB select => 방금 가입된 학생
-		
+		int id = student.getId();
+		Student latestStudent = studentBO.getStudentById(id);
 		// Model에 담기
-		
+		model.addAttribute("student", latestStudent);
 		// 하면 이동
 		return "lesson04/afterAddStudent";
 	}
