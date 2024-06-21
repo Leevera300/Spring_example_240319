166d38364b310a2c6db799ea6d4b3081541ac667
DESKTOP-SES5PE0\user <test@test.com>
2024-06-21 18:52:02 +0900

    240621_수업내용

---
+++ src/main/java/com/example/lesson05/Lesson05Controller.java
@@ -0,0 +1,53 @@
+package com.example.lesson05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/lesson05")
+@Controller
+public class Lesson05Controller {
+
+	@GetMapping("/ex01")
+	public String ex01() {
+		return "lesson05/ex01";
+	}
+	
+	
+	@GetMapping("/ex02")
+	public String ex02(Model model) {
+		// List<String> 
+		List<String> fruits = new ArrayList<>();
+		fruits.add("사과");
+		fruits.add("메론");
+		fruits.add("복숭아");
+		fruits.add("수박");
+		fruits.add("포도");
+		
+		model.addAttribute("fruits", fruits);
+		
+		// List<Map<String, Object>>
+		List<Map<String, Object>> users = new ArrayList<>();
+		Map<String, Object> user = new HashMap<>();
+		user.put("name", "Michael");
+		user.put("age", 23);
+		user.put("hobby", "basketball");
+		users.add(user);
+		
+		user = new HashMap<>();
+		user.put("name", "Marondal");
+		user.put("age", 15);
+		user.put("hobby", "ballet");
+		users.add(user);
+		
+		model.addAttribute("users", users);
+		
+		return "lesson05/ex02";
+	}
+}
